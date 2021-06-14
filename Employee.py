cee58ab7533dec994e3ef44d6053f1003528688c
Sravani87329 <84727760+Sravani87329@users.noreply.github.com>
2021-06-14 21:32:46 +0530

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -3,7 +3,7 @@ import sqlite3
 con = sqlite3.connect("employee.db")  
 print("Database opened successfully")  
   
-con.execute("create table if not exists Employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, address TEXT NOT NULL)")  
+con.execute("create table Employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, address TEXT NOT NULL)")  
   
 print("Table created successfully")  
   
